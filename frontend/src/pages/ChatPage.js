aa9d5d27552a0f29a5614506ac0b4c23cf015f97
UmeshGardas <umeshgardas123@gmail.com>
2025-08-07 13:11:54 +0530

    testing

--- frontend/src/pages/ChatPage.js
+++ frontend/src/pages/ChatPage.js
@@ -9,12 +9,7 @@ const ChatPage = () => {
 
     return (
         <div className="flex h-screen antialiased text-gray-800">
-            <div className="flex flex-row h-full w-full overflow-x-hidden">
-                {user && <ChatList onSelectChat={setSelectedChat} />}
-                {user && <ChatBox selectedChat={selectedChat} />}
-            </div>
-            {/* Optional: Add a logout button to the chat page */}
-            <button onClick={logout} className="absolute top-4 right-4 bg-red-500 text-white px-3 py-1 rounded">Logout</button>
+           Chatpage
         </div>
     );
 };
